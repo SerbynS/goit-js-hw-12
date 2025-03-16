bdc8c54080c29de8f8ddb7ab1af0983432df1431
SerbynS <serbin.sasha1@gmail.com>
2025-03-16 14:19:21 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,87 +1,3 @@
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-
-// iziToast.error({
-//   backgroundColor: '#EF4040',
-//   messageColor: '#FAFAFB',
-//   theme: 'dark',
-//   progressBarColor: '#B51B1B',
-//   maxWidth: '432',
-//   messageSize: '16',
-//   //   icon: '',
-//   position: 'topRight',
-//   message: `Sorry, there are no images matching your search query. Please, try again!`,
-// });
-// const form = document.querySelector('.form');
-// const gallery = document.querySelector('.gallery');
-
-// form.addEventListener('submit', event => {
-//   event.preventDefault();
-
-//   const search = event.target['search-text'].value;
-//   fetch(
-//     `https://pixabay.com/api/?key=22291580-fa60bbd7949edc60de7cbcd1e&q=${search}&image_type=photo&per_page=10`
-//   )
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log(data.hits);
-
-//       const galleryMarkup = data.hits
-//         .map(
-//           ({
-//             webformatURL,
-//             largeImageURL,
-//             likes,
-//             views,
-//             comments,
-//             downloads,
-//           }) =>
-//             `
-//           <li class="gallery-item">
-//         <a
-//           class="gallery-link"
-//           href="${webformatURL}"
-//         >
-//           <img
-//             class="gallery-image"
-//             src="${largeImageURL}"
-//             alt=""
-//           />
-//         </a>
-//         <ul class="info-cards">
-//           <li class="info-card">
-//             <h3>Likes</h3>
-//             <p>${likes}</p>
-//           </li>
-//           <li class="info-card">
-//             <h3>Views</h3>
-//             <p>${views}</p>
-//           </li>
-//           <li class="info-card">
-//             <h3>Comments</h3>
-//             <p>${comments}</p>
-//           </li>
-//           <li class="info-card">
-//             <h3>Downloads</h3>
-//             <p>${downloads}</p>
-//           </li>
-//         </ul>
-//       </li>
-//           `
-//         )
-//         .join('');
-
-//       gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-//     })
-//     .catch(error => console.log(error));
-// });
-
-import { space } from 'postcss/lib/list';
 import { fetchImages } from './js/pixabay-api.js';
 import {
   renderGalleryMarkup,
@@ -90,7 +6,7 @@ import {
   renderSimpleLightbox,
   renderError,
   toggleLoader,
-} from './js/render-funcions.js';
+} from './js/render-functions.js';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
